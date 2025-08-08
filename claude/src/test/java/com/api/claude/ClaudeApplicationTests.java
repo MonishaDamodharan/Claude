e9394686775e69cec037a46e3906c4c9952aed8b
MonishaDamodharan <140441754+MonishaDamodharan@users.noreply.github.com>
2025-08-08 22:28:46 +0530

    Base code

---
+++ claude/src/test/java/com/api/claude/ClaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.claude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
